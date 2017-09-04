7459d3b435118ddd6c6ad73b53b5774c69896608
joykare <warugujoy@gmail.com>
2017-09-04 18:52:17 +0300

    Add tests for search input population

--- src/LovableFilterableTable.test.js
+++ src/LovableFilterableTable.test.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { shallow } from "enzyme";
+import fs from "fs";
+import path from "path";
 import LoveableFilterbleTable from "./LovableFilterableTable";
 import { tableSchema } from "./App";
+
+const SAMPLE_RESPONSE_FILE = path.join(__dirname, "../sample-response.json");
+
+const genereteItems = () => {
+  const response = fs.readFileSync(SAMPLE_RESPONSE_FILE);
+  const json = JSON.parse(response);
+
+  return json.slice(0, 30).map(item => ({
+    ...item,
+    isLoved: false
+  }))
+}
 
 describe("LovableFilterableTable", () => {
   it("renders without crashing <smoke test>", () => {
@@ -66,7 +80,46 @@ describe("LovableFilterableTable", () => {
             </td>
           )
         ).toBe(true);
+      });
+    });
+  });
+
+  describe("user entering search query", () => {
+    let items;
+
+    beforeEach(() => {
+      items = genereteItems();
+      wrapper = shallow(
+        <LoveableFilterbleTable items={items} schema={tableSchema} />
+      );
+
+      const searchBox = wrapper.find("input");
+      searchBox.simulate("change", {target: {value: "coin"}});
+    });
+
+    it("render a subset of matching [items]", () => {
+      const matching = items.filter(item => item.name.match(/coin/i));
+      const notmatching = items.filter(item => !item.name.match(/coin/i));
+
+      matching.forEach(match => {
+        expect(
+          wrapper.containsMatchingElement(
+            <td>
+              {match.name}
+            </td>
+          )
+        ).toBe(true);
+      });
+
+      notmatching.forEach(match => {
+        expect(
+          wrapper.containsMatchingElement(
+            <td>
+              {match.name}
+            </td>
+          )
+        ).toBe(false);
       })
     });
-  })
-})
+  });
+});
